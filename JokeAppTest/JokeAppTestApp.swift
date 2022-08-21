ad4c92ef94637081b3123d47e7ddb02767f50e47
Oleksandr Filippov <ofilippov@prograils.com>
2022-08-21 16:58:03 +0200

    Initial Commit

---
+++ JokeAppTest/JokeAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeAppTestApp.swift
+//  JokeAppTest
+//
+//  Created by Oleksandr Filippov (work) on 21/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct JokeAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
